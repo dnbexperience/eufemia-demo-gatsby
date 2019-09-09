33e6dced0bcd1e9988930fcb548ae0157047278c
Tobias Høegh <tobias@tujo.no>
2019-09-09 19:25:54 +0200

    add some more gatsby plugins to extend functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,21 @@ module.exports = {
     title: 'Eufemia Demos'
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Eufemia Demos',
+        short_name: 'Eufemia',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#007272',
+        display: 'minimal-ui',
+        icon: 'node_modules/dnb-ui-lib/assets/images/dnb-icon.png' // This path is relative to the root of the site.
+      }
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    'gatsby-plugin-emotion'
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-offline'
   ]
 }
